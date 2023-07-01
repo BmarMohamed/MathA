fcccc9fa38235eb8cde8b8ef26a11898fcf799cd
BmarMohamed <bmar0mohamed@gmail.com>
2023-07-01 02:36:21 -0700

    add animation actions not_stable

--- src/animation.class.ts
+++ src/animation.class.ts
@@ -1,12 +1,12 @@
 import VisualElement from "./visual_element/visual_element.class.js";
 import { Render } from "./visual_element/default_properties.object.js";
+import Queue from "./lib/classes/data_structures/queue.class.js";
 
 class Frame {
     constructor() {
         this.id = Frame.frame_id++;
         this.actions = [];
     }
-
     private static frame_id : number = 0;
     private id! : number;
     private next! : Frame | null;
@@ -15,7 +15,9 @@ class Frame {
         event : string;
         params : any[];
     }[];
-
+    public getFrameId() {
+        return this.id;
+    }
     public setNextFrame(frame : Frame){
         return this.next = frame;
     }
@@ -50,6 +52,65 @@ class Frame {
     }
 }
 
+export enum AnimationActions {
+    Stop,
+    Pause,
+    Continue,
+    Goto,
+    Speed,
+}
+
+class AnimationAction {
+    constructor(action_type : AnimationActions, frame : number, params : {
+        duration? : number,
+        goto_frame? : number,
+        speed? : number
+    } = {}) {
+        this.frame = frame;
+        this.action_type = action_type;
+        this.params = params;
+    }
+    private frame! : number;
+    public action_type! : number;
+    private params! : {
+        duration? : number,
+        goto_frame? : number,
+        speed? : number
+    };
+    public getFrame() {
+        return this.frame;
+    }
+    public static stop() {
+        clearInterval(Animation.animation_interval!);
+    }
+    public static continue() {
+        Animation.start();
+    }
+    public static pause(duration : number) {
+        this.stop()
+        setTimeout(() => {
+            this.continue();
+        }, duration);
+    }
+    public static goto(goto_frame : number) {
+        this.stop();
+        Animation.current_frame = goto_frame
+        this.continue();
+    }
+    public static speed(speed : number) {
+        this.stop();
+        Animation.frame_duration /= speed;
+        this.continue();
+    }
+    public execute() {
+        if(this.action_type === AnimationActions.Stop) AnimationAction.stop();
+        else if(this.action_type === AnimationActions.Pause) AnimationAction.pause(this.params.duration || 0);
+        else if(this.action_type === AnimationActions.Continue) AnimationAction.continue();
+        else if(this.action_type === AnimationActions.Goto) AnimationAction.goto(this.params.goto_frame || 0);
+        else if(this.action_type === AnimationActions.Speed) AnimationAction.speed(this.params.speed || 1);
+    }
+}
+
 class Animation {
     constructor() {}
 //===============================================================================================
@@ -58,6 +119,7 @@ class Animation {
     private static resolution : [ number, number ] = [ 1280, 720 ];
     private static background : string = "#000000";
     private static parent : HTMLElement = document.body;
+    private static animation_actions_queue : Queue<AnimationAction>;
 
 //===============================================================================================
     public static getProperties() {
@@ -92,6 +154,7 @@ class Animation {
         this.initializeFrames();
         Render.width = this.getProperties().resolution[0];
         Render.height = this.getProperties().resolution[1];
+        this.animation_actions_queue = new Queue();
     }
 //===============================================================================================  
     private static html : HTMLDivElement;
@@ -112,6 +175,14 @@ class Animation {
     public static at(index : number) {
         return this.frames[index] || this.frames[0]!;
     }
+    public static addAnimationAction(action_type : AnimationActions, frame : number, params : {
+        duration? : number,
+        goto_frame? : number,
+        speed? : number
+    } = {}) {
+        let action = new AnimationAction(action_type, frame, params);
+        this.animation_actions_queue.queue(action);
+    }
     private static initializeFrames() {
         for(let i = 0 ; i < this.fps * this.duration; i++) this.frames.push(new Frame());
         for(let i = 0 ; i < this.fps * this.duration; i++) 
@@ -119,15 +190,29 @@ class Animation {
                 this.at(i + 1)
             );
     }
+    public static executeAnimationActions(frame_number : number) {
+        if(this.animation_actions_queue.length === 0) return;
+        if(this.animation_actions_queue.getCurrent().getFrame() === frame_number) {
+            this.animation_actions_queue.dequeue()!.execute();
+            this.executeAnimationActions(frame_number);
+        }
+    }
 //===============================================================================================
+    public static animation_interval : number | null;
+    public static current_frame : number = 0;
+    public static frame_duration : number = 1000 / Animation.fps;
     public static start() {
-        let frame = this.at(0)
-        const interval = setInterval(() => {
+        let frame = this.frames[this.current_frame];
+        this.animation_interval = setInterval(() => {
             frame.prepare();
             frame.execute();
-            if(frame !== frame.getNextFrame()) frame = frame.getNextFrame();
-            else clearInterval(interval);
-        }, 1000 / Animation.fps);
+            this.executeAnimationActions(frame.getFrameId());
+            if(frame !== frame.getNextFrame()) {
+                frame = frame.getNextFrame();
+                this.current_frame = frame.getFrameId();
+            }
+            else AnimationAction.stop();
+        }, this.frame_duration);
     }
 //===============================================================================================
 
---
+++ src/visual_element/events/font.event.ts
@@ -0,0 +1,7 @@
+import Events from "./event.js";
+
+const FontEvents : Events = {
+
+}
+
+export default FontEvents;
